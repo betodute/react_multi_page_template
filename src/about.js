7a810855d347e673f2da93aa22bdb7db3e2957e3
Alberto Duarte <betodute@gmail.com>
2022-08-01 21:57:54 -0700

    File Structure Complete

---
+++ src/about.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <Template>
+      <h1> About Alberto </h1>
+    </Template>
+  )
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ src/script.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <Template>
+      <h1> Alberto's Portfolio </h1>
+    </Template>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
